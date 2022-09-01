60301ed3fcab242ec247889dc14e27ec40aa5af9
sarkermajid <majidursarkar333@gmail.com>
2022-09-01 09:02:40 +0600

    Convert Html To Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
+import { CenterComponent } from './center/center.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    LeftComponent,
+    RightComponent,
+    CenterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
